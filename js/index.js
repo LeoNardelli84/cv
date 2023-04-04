b6286c7c04ba9c945570962ca1f5fa938863097b
LeoNardelli84 <siblings.distribuidora@gmail.com>
2023-04-04 19:31:26 -0300

    testeando animaciones con scroll

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
 
 AOS.init();
-var scroll = $(window).scrollTop();
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
-
+    console.log(scroll);
     $(".info").removeClass("invisible");
-    $(".info").addClass("animate__zoomIn");
+    $(".info").addClass("animate__bounceIn");
     $(".parrafo").addClass("animate__zoomIn");
-    $(".img-fondo").addClass("animate__bounceInRight");
-
-    if (scroll >= 70) {
-        
-        //ejecuta una funcion info2
-    } else {
-
-    }
+    
+    switch (scroll){
+        case 80:
+            //codigo aca;
+            break;
+        case 100: 
+            //codigo
+            break;
+        case 120: 
+            $(".info2").removeClass("invisible");
+            $(".info2").addClass("animate__bounceInLeft");
+            console.log(scroll);
+            break;
+        case 780:
+             $(".foto-section3").addClass("animate__bounceInRight");
+            //codigo
+            break;
+    }//end switch scroll
+    
 });
