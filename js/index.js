ba5fdf3645f8736b9c1d5204a94b7ace7c8298ab
LeoNardelli84 <siblings.distribuidora@gmail.com>
2023-04-05 00:12:00 -0300

    footer color

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 
 AOS.init();
 
+//para conectar con metamask
+//https://www.youtube.com/watch?v=DXRSQGvWM8M 
+
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     console.log(scroll);
@@ -16,12 +19,12 @@ $(window).scroll(function (event) {
             //codigo
             break;
         case 120, 121, 122, 123, 124, 125, 126,
-             127, 128, 129, 130: 
+             127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140: 
             $(".info2").removeClass("invisible");
             $(".info2").addClass("animate__bounceInLeft");
            
             //aca testeo lo que hace la variable scroll
-            console.log(scroll);
+            //console.log(scroll);
             break;
 
     }//end switch scroll
